c081f344873e196fc31e25d304530a6feda2b58e
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2026-04-04 21:20:12 -0400

    Make single release workflow per repo

--- sheepdogasciidoc.parent/scripts/release.bat
+++ sheepdogasciidoc.parent/scripts/release.bat
@@ -2,6 +2,12 @@ cd ..
 call git reset --hard HEAD
 call git clean -fdx
 call git pull
-call gradle release
+call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=false -DallowDowngrade=true
+call mvn org.farhan:sheep-dog-mgmt-maven-plugin:prepare -DpreparationGoals=deploy,-DskipTests
 call git push
+call git push --tags
+call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=true
+call git clean -fdx
+git diff --quiet HEAD -- . ":(exclude)*.bat" && (echo No dependency changes to commit) || (call git add * && call git commit -m "Upgrading dependency versions" && call git push)
+call mvn clean deploy -DskipTests
 cd scripts
