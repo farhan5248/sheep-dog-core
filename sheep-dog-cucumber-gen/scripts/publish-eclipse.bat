fdd20de56339660d044a3b6f0763825e4ca6c8f1
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2026-04-09 23:22:33 -0400

    Repoint Maven and Docker to Nexus

--- sheep-dog-cucumber-gen/scripts/publish-eclipse.bat
+++ sheep-dog-cucumber-gen/scripts/publish-eclipse.bat
@@ -1,35 +1,37 @@
+@echo off
+REM Publish Eclipse OSGi artifacts to Nexus that aren't available on Maven Central.
+REM These are transitive dependencies of the Xtext/EMF/UML2 stack with Eclipse
+REM OSGi-style version numbers (e.g. 2.35.0.v20230829-0934).
+REM
+REM Prereqs:
+REM   - settings.xml has a nexus-releases server entry with helm-deployer credentials
+REM   - mkcert root CA imported (for HTTPS to nexus.sheepdog.io)
+REM   - Run from the sheep-dog-cucumber-gen/scripts directory
+REM
+REM Step 1: copy all transitive deps into target/dependency
+REM Step 2: deploy the Eclipse-only jars to Nexus maven-releases
+
 cd ..
 call mvn clean
 call mvn dependency:copy-dependencies -DoutputDirectory=target/dependency
-call mvn deploy:deploy-file -Dfile=target/dependency/runtime-3.29.0.v20230726-0617.jar -Durl=https://maven.pkg.github.com/farhan5248/sheep-dog-local -DrepositoryId=github -DgroupId=org.eclipse.core -DartifactId=runtime -Dversion=3.29.0.v20230726-0617 
-call mvn deploy:deploy-file -Dfile=target/dependency/ecore-2.35.0.v20230829-0934.jar -Durl=https://maven.pkg.github.com/farhan5248/sheep-dog-local -DrepositoryId=github -DgroupId=org.eclipse.emf -DartifactId=ecore -Dversion=2.35.0.v20230829-0934 
-call mvn deploy:deploy-file -Dfile=target/dependency/ecore.xmi-2.35.0.v20230801-1141.jar -Durl=https://maven.pkg.github.com/farhan5248/sheep-dog-local -DrepositoryId=github -DgroupId=org.eclipse.emf -DartifactId=ecore.xmi -Dversion=2.35.0.v20230801-1141 
-call mvn deploy:deploy-file -Dfile=target/dependency/mapping.ecore2xml-2.12.0.v20230211-1150.jar -Durl=https://maven.pkg.github.com/farhan5248/sheep-dog-local -DrepositoryId=github -DgroupId=org.eclipse.emf -DartifactId=mapping.ecore2xml -Dversion=2.12.0.v20230211-1150 
-call mvn deploy:deploy-file -Dfile=target/dependency/common-2.29.0.v20230916-0637.jar -Durl=https://maven.pkg.github.com/farhan5248/sheep-dog-local -DrepositoryId=github -DgroupId=org.eclipse.emf -DartifactId=common -Dversion=2.29.0.v20230916-0637 
-call mvn deploy:deploy-file -Dfile=target/dependency/common-3.18.100.v20230730-1117.jar -Durl=https://maven.pkg.github.com/farhan5248/sheep-dog-local -DrepositoryId=github -DgroupId=org.eclipse.equinox -DartifactId=common -Dversion=3.18.100.v20230730-1117 
-call mvn deploy:deploy-file -Dfile=target/dependency/runtime-3.2.0.v20220404-1927.jar -Durl=https://maven.pkg.github.com/farhan5248/sheep-dog-local -DrepositoryId=github -DgroupId=org.antlr -DartifactId=runtime -Dversion=3.2.0.v20220404-1927 
-call mvn deploy:deploy-file -Dfile=target/dependency/xtext-2.32.0.v20230827-1315.jar -Durl=https://maven.pkg.github.com/farhan5248/sheep-dog-local -DrepositoryId=github -DgroupId=org.eclipse.xtext -DartifactId=xtext -Dversion=2.32.0.v20230827-1315 
-call mvn deploy:deploy-file -Dfile=target/dependency/util-2.32.0.v20230827-1315.jar -Durl=https://maven.pkg.github.com/farhan5248/sheep-dog-local -DrepositoryId=github -DgroupId=org.eclipse.xtext -DartifactId=util -Dversion=2.32.0.v20230827-1315 
-call mvn deploy:deploy-file -Dfile=target/dependency/xbase.lib-2.33.0.v20231121-0955.jar -Durl=https://maven.pkg.github.com/farhan5248/sheep-dog-local -DrepositoryId=github -DgroupId=org.eclipse.xtext -DartifactId=xbase.lib -Dversion=2.33.0.v20231121-0955 
-call mvn deploy:deploy-file -Dfile=target/dependency/uml.profile.standard-1.5.0.v20221116-1811.jar -Durl=https://maven.pkg.github.com/farhan5248/sheep-dog-local -DrepositoryId=github -DgroupId=org.eclipse.uml2 -DartifactId=uml.profile.standard -Dversion=1.5.0.v20221116-1811 
-call mvn deploy:deploy-file -Dfile=target/dependency/common-2.5.0.v20221116-1811.jar -Durl=https://maven.pkg.github.com/farhan5248/sheep-dog-local -DrepositoryId=github -DgroupId=org.eclipse.uml2 -DartifactId=common -Dversion=2.5.0.v20221116-1811 
-call mvn deploy:deploy-file -Dfile=target/dependency/types-2.5.0.v20221116-1811.jar -Durl=https://maven.pkg.github.com/farhan5248/sheep-dog-local -DrepositoryId=github -DgroupId=org.eclipse.uml2 -DartifactId=types -Dversion=2.5.0.v20221116-1811 
-call mvn deploy:deploy-file -Dfile=target/dependency/uml.resources-5.5.0.v20221116-1811.jar -Durl=https://maven.pkg.github.com/farhan5248/sheep-dog-local -DrepositoryId=github -DgroupId=org.eclipse.uml2 -DartifactId=uml.resources -Dversion=5.5.0.v20221116-1811 
-call mvn deploy:deploy-file -Dfile=target/dependency/uml-5.5.0.v20210228-1829.jar -Durl=https://maven.pkg.github.com/farhan5248/sheep-dog-local -DrepositoryId=github -DgroupId=org.eclipse.uml2 -DartifactId=uml -Dversion=5.5.0.v20210228-1829 
-cd scripts
 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+set NEXUS_URL=https://nexus.sheepdog.io/repository/maven-releases/
+set REPO_ID=nexus-releases
+
+REM --- org.eclipse.core ---
+call mvn deploy:deploy-file -Dfile=target/dependency/runtime-3.29.0.v20230726-0617.jar -Durl=%NEXUS_URL% -DrepositoryId=%REPO_ID% -DgroupId=org.eclipse.core -DartifactId=runtime -Dversion=3.29.0.v20230726-0617
+
+REM --- org.eclipse.emf ---
+call mvn deploy:deploy-file -Dfile=target/dependency/ecore-2.35.0.v20230829-0934.jar -Durl=%NEXUS_URL% -DrepositoryId=%REPO_ID% -DgroupId=org.eclipse.emf -DartifactId=ecore -Dversion=2.35.0.v20230829-0934
+call mvn deploy:deploy-file -Dfile=target/dependency/ecore.xmi-2.35.0.v20230801-1141.jar -Durl=%NEXUS_URL% -DrepositoryId=%REPO_ID% -DgroupId=org.eclipse.emf -DartifactId=ecore.xmi -Dversion=2.35.0.v20230801-1141
+call mvn deploy:deploy-file -Dfile=target/dependency/mapping.ecore2xml-2.12.0.v20230211-1150.jar -Durl=%NEXUS_URL% -DrepositoryId=%REPO_ID% -DgroupId=org.eclipse.emf -DartifactId=mapping.ecore2xml -Dversion=2.12.0.v20230211-1150
+call mvn deploy:deploy-file -Dfile=target/dependency/common-2.29.0.v20230916-0637.jar -Durl=%NEXUS_URL% -DrepositoryId=%REPO_ID% -DgroupId=org.eclipse.emf -DartifactId=common -Dversion=2.29.0.v20230916-0637
+
+REM --- org.eclipse.uml2 ---
+call mvn deploy:deploy-file -Dfile=target/dependency/uml.profile.standard-1.5.0.v20221116-1811.jar -Durl=%NEXUS_URL% -DrepositoryId=%REPO_ID% -DgroupId=org.eclipse.uml2 -DartifactId=uml.profile.standard -Dversion=1.5.0.v20221116-1811
+call mvn deploy:deploy-file -Dfile=target/dependency/common-2.5.0.v20221116-1811.jar -Durl=%NEXUS_URL% -DrepositoryId=%REPO_ID% -DgroupId=org.eclipse.uml2 -DartifactId=common -Dversion=2.5.0.v20221116-1811
+call mvn deploy:deploy-file -Dfile=target/dependency/types-2.5.0.v20221116-1811.jar -Durl=%NEXUS_URL% -DrepositoryId=%REPO_ID% -DgroupId=org.eclipse.uml2 -DartifactId=types -Dversion=2.5.0.v20221116-1811
+call mvn deploy:deploy-file -Dfile=target/dependency/uml.resources-5.5.0.v20221116-1811.jar -Durl=%NEXUS_URL% -DrepositoryId=%REPO_ID% -DgroupId=org.eclipse.uml2 -DartifactId=uml.resources -Dversion=5.5.0.v20221116-1811
+call mvn deploy:deploy-file -Dfile=target/dependency/uml-5.5.0.v20210228-1829.jar -Durl=%NEXUS_URL% -DrepositoryId=%REPO_ID% -DgroupId=org.eclipse.uml2 -DartifactId=uml -Dversion=5.5.0.v20210228-1829
+
+cd scripts
